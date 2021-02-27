19d44423ce1b53af30c10358561e2ff785f23c71
samcallen11 <56631431+samcallen11@users.noreply.github.com>
2021-02-27 16:20:10 +0330

    main with loading screen
    click on the celebrity tour in order to open the main page :)

--- main.dart
+++ main.dart
@@ -1,4 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Named Routes Demo',
+    // Start the app with the "/" named route. In this case, the app starts
+    // on the FirstScreen widget.
+    initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => FirstScreen(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => SecondScreen(),
+    },
+  ));
+}
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: Stack(
+        children:[
+          Container(
+            child:  Center(
+              child: SpinKitCubeGrid(
+                color: Colors.black,
+                size: 80.0,
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+
+            child: RaisedButton(
+                color: Colors.white,
+
+                elevation: 0,
+                hoverElevation: 0,
+                focusElevation: 0,
+                highlightElevation: 0,
+
+
+                child: Text("Celebrity tour", style: TextStyle(fontSize: 25),),
+
+
+                onPressed: () {
+                  // Navigate to the second screen using a named route.
+                  Navigator.pushNamed(context, '/second');
+                }),
+          ),
+        ],
+      ),
+
+
+
+      //Navigator.pushNamed(context, '/home');
+
+    );
+  }
+}
+
+
+
+
+
 
 void gallery(){
   print("gallery");
@@ -8,21 +83,20 @@ void gallery(){
 void camera(){
   print("camera");
 }
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ),);
-}
-class Home extends StatefulWidget {
+
+
+class SecondScreen extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _SecondScreenState createState() => _SecondScreenState();
 }
 
-class _HomeState extends State<Home> {
+class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
-
-    double width = MediaQuery. of(context). size. width;
+    double width = MediaQuery
+        .of(context)
+        .size
+        .width;
     print(width);
     return Scaffold(
       backgroundColor: Colors.black,
@@ -36,17 +110,19 @@ class _HomeState extends State<Home> {
           Align(
             alignment: Alignment.bottomLeft,
             child: Container(
-               child: ButtonTheme(
-                   minWidth: width/2,
-                    height: 50.0,
-                    child: RaisedButton(
-                    onPressed: (){
-                      gallery();
+              child: ButtonTheme(
+                minWidth: width / 2,
+                height: 50.0,
+                child: RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        gallery();
+                      });
                     },
                     color: Colors.blueGrey[600],
                     child: Icon(Icons.photo_album)
                 ),
-               ),
+              ),
             ),
 
           ),
@@ -55,15 +131,17 @@ class _HomeState extends State<Home> {
             child: Container(
 
               child: ButtonTheme(
-                minWidth: width/2,
+                minWidth: width / 2,
                 height: 50.0,
                 child: RaisedButton(
-                onPressed: (){
-                  camera();
-                },
-                color: Colors.blueGrey[600],
-                child: Icon(Icons.camera)
-              ),
+                    onPressed: () {
+                      setState(() {
+                        camera();
+                      });
+                    },
+                    color: Colors.blueGrey[600],
+                    child: Icon(Icons.camera)
+                ),
               )
 
               ,
@@ -71,10 +149,10 @@ class _HomeState extends State<Home> {
 
           ),
         ],
-        ),
+      ),
 
     );
   }
+
+
 }
-
-
